5688e3436b46e0ed1e11ee49807f1920308ca513
Emre Konar <m.emre.konar@gmail.com>
2025-03-05 02:12:48 +0100

    feat(scraper): initialized scraper as microservices

--- packages/scraper/src/main.ts
+++ packages/scraper/src/main.ts
@@ -1,8 +1,31 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const host = process.env.RABBITMQ_HOST!;
+  const port = process.env.RABBITMQ_PORT!;
+  const user = process.env.RABBITMQ_USER!;
+  const pw = process.env.RABBITMQ_PW!;
+  const queue = process.env.RABBITMQ_QUEUE!;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://${user}:${pw}@${host}:${port}`],
+        queue,
+        queueOptions: { durable: true },
+      },
+    },
+  );
+
+  const configService = app.get(ConfigService);
+  console.log(
+    `Scraper Microservice listening on queue: ${configService.get('RABBITMQ_QUEUE')}`,
+  );
+
+  await app.listen();
 }
 bootstrap();
