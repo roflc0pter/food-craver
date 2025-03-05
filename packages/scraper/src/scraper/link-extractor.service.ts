95a531e47e8a3df9d8b26bd653604d74c9961247
Emre Konar <m.emre.konar@gmail.com>
2025-03-05 02:13:51 +0100

    feat(scraper): added  LinkExtractor services

---
+++ packages/scraper/src/scraper/link-extractor.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Page } from 'puppeteer';
+
+@Injectable()
+export class LinkExtractorService {
+  async extractLinks(page: Page, baseUrl: string): Promise<string[]> {
+    const rawLinks = await this.getRawLinks(page);
+    const bla = this.filterValidLinks(rawLinks, baseUrl);
+    return bla;
+  }
+
+  private async getRawLinks(page: Page) {
+    return page.evaluate(() => {
+      return Array.from(document.querySelectorAll('a')).map((el) =>
+        el.getAttribute('href'),
+      );
+    });
+  }
+
+  private filterValidLinks(
+    rawLinks: (string | null)[],
+    baseUrl: string,
+  ): string[] {
+    const baseDomain = new URL(baseUrl).hostname;
+    const filteredLinks = new Set<string>();
+
+    for (const link of rawLinks) {
+      if (!link) {
+        continue;
+      }
+      const absoluteUrl = new URL(link, baseUrl).href;
+      if (this.isValidLink(absoluteUrl, baseDomain)) {
+        filteredLinks.add(absoluteUrl);
+      }
+    }
+    return Array.from(filteredLinks);
+  }
+
+  private isValidLink(url: string, baseDomain: string): boolean {
+    return new URL(url).hostname === baseDomain;
+  }
+}
